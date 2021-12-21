1b54abd51c428f9e7ca3cb049b0e3de18a0c35ce
Estesq <rohan.pawar@helixstack.in>
2021-12-21 20:10:09 +0530

    added progressbar element

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import TemporaryDrawer from './components/Sidebar/Sidebar';
 import ColorToggleButton from './components/Toggle/Toggle'
-// import GroupedButtons from './components/Inputbutton/Inputbutton';
-// import LinearDeterminate from './components/ProgressBar/ProgressBar';
+import GroupedButtons from './components/Inputbutton/Inputbutton';
+import LinearDeterminate from './components/ProgressBar/ProgressBar';
 import CustomizedSwitches from './components/Switch/Switch'
 function App() {
   return (
     <div className="App">
       {/* <TemporaryDrawer />                 */}
       {/* <LinearDeterminate className="progressbar" /> */}
-      <ColorToggleButton />
-      {/* <CustomizedSwitches /> */}
+      {/* <ColorToggleButton /> */}
+      {/* <CustomizedSwitches11 /> */}
+      <GroupedButtons />
     </div>
   );
 }
--- src/components/Inputbutton/Inputbutton.js
+++ src/components/Inputbutton/Inputbutton.js
@@ -4,6 +4,7 @@ import { Box } from '@mui/system';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import './Inputbutton.scss';
+import Typography from '@mui/material/Typography';
 
 
 class GroupedButtons extends React.Component {
@@ -20,11 +21,18 @@ class GroupedButtons extends React.Component {
         // const displayCounter = this.state.counter > 0;
 
         return (
-            <Box sx={{ border: 1, borderRadius: 2, p: 1, width: '300px' }}>
-                <Button variant="outlined" onClick={this.handleDecrement} className="btn-icon" > <RemoveIcon /></Button>
-                <Button className="year-counter" disabled> {this.state.counter}</Button>
-                <Button variant="outlined" onClick={this.handleIncrement} className="btn-icon"> <AddIcon /></Button>
-            </Box>
+
+                <Box sx={{ border: 1, borderRadius: 2, p: 1, width: '300px' }}>
+                    <Typography variant="h6" component="h6" className="display-inline mr-5">
+                        Name
+                    </Typography>
+                    <div className="display-inline">
+                        <Button variant="outlined" onClick={this.handleDecrement} className="btn-icon" > <RemoveIcon /></Button>
+                        <Button className="year-counter" disabled> {this.state.counter}</Button>
+                        <Button variant="outlined" onClick={this.handleIncrement} className="btn-icon"> <AddIcon /></Button>
+                    </div>
+                </Box>
+
         );
     }
 }
